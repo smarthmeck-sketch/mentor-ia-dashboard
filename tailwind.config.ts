2fa1508df66ed7c240e9dd01ee4b9ee677746335
smarthmeck-sketch <smarthmeck@gmail.com>
2025-11-14 04:01:14 -0800

    Add Tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config }  'tailwindcss'
+
+const config: Config = {
+  darkMode: 'class',
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: '#050814',
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config
