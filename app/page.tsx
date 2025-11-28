5dc3d5fb1d4b3b8b4e95d68efcca0309cfc42c94
smarthmeck-sketch <smarthmeck@gmail.com>
2025-11-28 14:05:04 -0300

    Update page.tsx
    Fix 'use client' directive placement - must be on line 1 before imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
+'use client';
+
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-
-'use client';
 
 export default function Home() {
   const [email, setEmail] = useState('');
